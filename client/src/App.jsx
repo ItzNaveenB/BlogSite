a96c31155b6bb574099d5b25a24710d90cc6949e
Naveen <naveenbaghel5429@gmail.com>
2024-01-16 22:26:01 +0530

    making header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Search from './pages/Search';
-
-function App() {
-  return 
-    <>
-      <BrowserRouter className='text-3xl'>
-        <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/search' element={<Search />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+import SignIn from "./pages/SignIn.jsx"
+import SignUp from "./pages/SignUp.jsx"
+import Header from "./components/Header";
+export default function(){
+  return(
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/sign-in" element={<SignIn />}/>
+        <Route path="/sign-up" element={<SignUp />}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App
