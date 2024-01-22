be9cfeab50f1e37222dc84c072b93ba6e03c7a3a
Naveen <naveenbaghel5429@gmail.com>
2024-01-22 18:35:45 +0530

    Connect backend with Database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from "express"
-const app = express()
+import mongoose from "mongoose"
+import dotenv from 'dotenv'
 
-app.listen(3000,()=> {
+dotenv.config();
+const app = express();
+
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('MOngoDb is connected')
+    }).catch((err) => {
+        console.error(err);
+    })
+
+
+app.listen(3000, () => {
     console.log(`Server is running......`)
 })
