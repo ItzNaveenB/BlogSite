185ee43ea47fb424b385dd83a6ed1fcc188119dd
Naveen <naveenbaghel5429@gmail.com>
2024-01-24 10:02:54 +0530

    add functionality to the signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,42 +1,42 @@
-// import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 // import OAuth from '../components/OAuth';
 
 export default function SignUp() {
-  // const [formData, setFormData] = useState({});
-  // const [errorMessage, setErrorMessage] = useState(null);
-  // const [loading, setLoading] = useState(false);
-  // const navigate = useNavigate();
-  // const handleChange = (e) => {
-  //   setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
-  // };
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   if (!formData.username || !formData.email || !formData.password) {
-  //     return setErrorMessage('Please fill out all fields.');
-  //   }
-  //   try {
-  //     setLoading(true);
-  //     setErrorMessage(null);
-  //     const res = await fetch('/api/auth/signup', {
-  //       method: 'POST',
-  //       headers: { 'Content-Type': 'application/json' },
-  //       body: JSON.stringify(formData),
-  //     });
-  //     const data = await res.json();
-  //     if (data.success === false) {
-  //       return setErrorMessage(data.message);
-  //     }
-  //     setLoading(false);
-  //     if(res.ok) {
-  //       navigate('/sign-in');
-  //     }
-  //   } catch (error) {
-  //     setErrorMessage(error.message);
-  //     setLoading(false);
-  //   }
-  // };
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -56,14 +56,14 @@ export default function SignUp() {
         {/* right */}
 
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value='Your username' />
               <TextInput
                 type='text'
                 placeholder='Username'
                 id='username'
-              // onChange={handleChange}
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -72,7 +72,7 @@ export default function SignUp() {
                 type='email'
                 placeholder='name@company.com'
                 id='email'
-              // onChange={handleChange}
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -81,13 +81,13 @@ export default function SignUp() {
                 type='password'
                 placeholder='Password'
                 id='password'
-              // onChange={handleChange}
+                onChange={handleChange}
               />
             </div>
-            {/* <Button
+            <Button
               gradientDuoTone='purpleToPink'
               type='submit'
-              // disabled={loading}
+              disabled={loading}
             >
               {loading ? (
                 <>
@@ -97,20 +97,20 @@ export default function SignUp() {
               ) : (
                 'Sign Up'
               )}
-            </Button> */}
-            
+            </Button>
+            {/* <OAuth /> */}
           </form>
-          {/* <div className='flex gap-2 text-sm mt-5'>
+          <div className='flex gap-2 text-sm mt-5'>
             <span>Have an account?</span>
             <Link to='/sign-in' className='text-blue-500'>
               Sign In
             </Link>
-          </div> */}
-          {/* {errorMessage && (
+          </div>
+          {errorMessage && (
             <Alert className='mt-5' color='failure'>
               {errorMessage}
             </Alert>
-          )} */}
+          )}
         </div>
       </div>
     </div>
