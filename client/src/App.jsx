5bd937f50b26381628e82a6b3b3017f39da31629
Naveen <naveenbaghel5429@gmail.com>
2024-01-24 22:01:49 +0530

    make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,25 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import SignIn from "./pages/SignIn.jsx"
 import SignUp from "./pages/SignUp.jsx"
 import Header from "./components/Header";
 import FooterCom from "./components/Footer.jsx";
-export default function(){
-  return(
+import Dashboard from "./pages/Dashboard.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+export default function () {
+  return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/sign-in" element={<SignIn />}/>
-        <Route path="/sign-up" element={<SignUp />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={< PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/" element={<SignUp />} />
       </Routes>
       <FooterCom />
     </BrowserRouter>
