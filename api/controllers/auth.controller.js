73fd84ebc5582cc49fede073754a87dbccb9cc2e
Naveen <naveenbaghel5429@gmail.com>
2024-01-24 09:48:00 +0530

    add ui to the sign-up page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,31 +1,34 @@
 import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 export const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    if (
-        !username ||
-        !email ||
-        !password ||
-        username === '' ||
-        email === '' ||
-        password === ''
-    ) {
-        //   next(errorHandler(400, 'All fields are required'));
-        res.send({ message: "something is wrong" });
-    }
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400, 'All fields are required'));
+  }
 
-    // const hashedPassword = bcryptjs.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-    const newUser = new User({
-        username,
-        email,
-        password,
-    });
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
-    try {
-        await newUser.save();
-        res.json('Signup successful');
-    } catch (error) {
-        res.send(error);
-    }
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    next(error);
+  }
 };
